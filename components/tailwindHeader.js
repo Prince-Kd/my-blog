aa2726e27f70b9b9984d49916e07824e0f60f67e
Mawuli Kwadzofio <princekd131@gmail.com>
2022-01-24 22:16:04 +0000

    Update tailwindHeader.js

--- components/tailwindHeader.js
+++ components/tailwindHeader.js
@@ -47,7 +47,7 @@ export default function Example() {
     }).then((willDelete) => {
       if (willDelete) {
         logOut().then(() => {
-          router.push('/');
+          router.push("/");
         });
       }
     });
@@ -58,10 +58,10 @@ export default function Example() {
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2  lg:px-2">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="relative flex flex-row items-center justify-between h-16">
+              <div className="flex items-center lg:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-purple-500 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="flex items-center justify-center p-2 rounded-md text-purple-500 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -70,33 +70,31 @@ export default function Example() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-around ">
-                <div className="flex-shrink-0 flex items-center">
-                  <Link href={"/"}>
-                    <a className=" flex items-center justify-center text-xl lg:text-3xl text-purple-500 font-bold">
-                      BLOGGER SPACE
-                    </a>
-                  </Link>
-                </div>
-                <div className="hidden sm:block sm:ml-6 ">
-                  <div className="flex space-x-4 lg:justify-center lg:items-center">
-                    {navigation.map((item) => (
-                      <Link href={item.href} key={item.name}>
-                        <a
-                          className={`mx-2 ${
-                            router.pathname == item.href
-                              ? "border-2 border-purple-500 rounded-md p-1"
-                              : ""
-                          }`}
-                        >
-                          {item.name}
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
+              <div className="flex-shrink-0">
+                <Link href={"/"}>
+                  <a className=" flex items-center justify-center text-xl lg:text-3xl text-purple-500 font-bold">
+                    BLOGGER SPACE
+                  </a>
+                </Link>
+              </div>
+              <div className="hidden sm:block sm:ml-6 ">
+                <div className="flex space-x-4 lg:justify-center lg:items-center">
+                  {navigation.map((item) => (
+                    <Link href={item.href} key={item.name}>
+                      <a
+                        className={`mx-2 ${
+                          router.pathname == item.href
+                            ? "border-2 border-purple-500 rounded-md p-1"
+                            : ""
+                        }`}
+                      >
+                        {item.name}
+                      </a>
+                    </Link>
+                  ))}
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="flex items-center pr-2">
                 {/* <button
                   type="button"
                   className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
@@ -125,20 +123,20 @@ export default function Example() {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(
-                      router.pathname == item.href
-                        ? "bg-gray-200 text-purple-500 border-l-4 border-l-purple-500"
-                        : "text-gray-500 hover:bg-gray-700 hover:text-purple-500",
-                      "block px-3 py-2 rounded-md text-base font-medium"
-                    )}
-                    aria-current={item.current ? "page" : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    router.pathname == item.href
+                      ? "bg-gray-200 text-purple-500 border-l-4 border-l-purple-500"
+                      : "text-gray-500 hover:bg-gray-700 hover:text-purple-500",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
               ))}
             </div>
           </Disclosure.Panel>
