fcf6c907e4365d968d8b9f9b4d3af769bac2c731
Mawuli Kwadzofio <princekd131@gmail.com>
2022-01-24 22:04:38 +0000

    Update tailwindHeader.js

--- components/tailwindHeader.js
+++ components/tailwindHeader.js
@@ -61,7 +61,7 @@ export default function Example() {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-purple-500 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -70,7 +70,7 @@ export default function Example() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-1 flex items-center justify-around ">
                 <div className="flex-shrink-0 flex items-center">
                   <Link href={"/"}>
                     <a className=" flex items-center justify-center text-xl lg:text-3xl text-purple-500 font-bold">
@@ -112,7 +112,7 @@ export default function Example() {
                   <Link href={"/log_in"}>
                     <a
                       type="button"
-                      className="bg-purple-500 rounded-md h-8 p-1 text-white font-medium text-center hover:h-9 mr-2 w-24"
+                      className="bg-purple-500 rounded-md h-8 p-1 text-white font-medium text-center hover:h-9 ml-2 w-20 lg:w-24"
                     >
                       LOG IN
                     </a>
